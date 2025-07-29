18403d5273d33a4e78c400eb34c9cf745530c22b
Ibarra1812 <ibarraalvaro852@gmail.com>
2025-07-29 20:51:43 -0300

    add: agregarAmigo function

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo() {
+    let nombre = document.querySelector('#amigo').value;
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre');
+        return;
+    }
+    
+    amigos.push(nombre);
+    
+    document.querySelector('#amigo').value = '';
+}
